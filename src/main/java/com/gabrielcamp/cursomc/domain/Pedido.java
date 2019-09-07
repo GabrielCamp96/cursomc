edccf7dc76b360eb99e5bfbc1346097be92208f1
Gabriel Campanário <gabriel.camp96@hotmail.com>
2019-09-06 21:28:15 -0300

    Item_Pedido criado e devidamente relacionado

--- src/main/java/com/gabrielcamp/cursomc/domain/Pedido.java
+++ src/main/java/com/gabrielcamp/cursomc/domain/Pedido.java
@@ -1,9 +1,9 @@
 package com.gabrielcamp.cursomc.domain;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -12,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -32,7 +33,8 @@ public class Pedido implements Serializable{
 	@JoinColumn(name="endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
 	
-//	private List<Produto> itens = new ArrayList<>();
+	@OneToMany(mappedBy="id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
 	
 	// cascade - Evita erro de entidade transiente (desconhecido do banco de dados e do Entity Manager)
 	@OneToOne(cascade=CascadeType.ALL, mappedBy="pedido")
@@ -51,6 +53,14 @@ public class Pedido implements Serializable{
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
 
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+	
 	public Integer getId() {
 		return id;
 	}
