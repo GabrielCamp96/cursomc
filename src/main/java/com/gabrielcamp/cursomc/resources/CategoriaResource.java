555f750e3326d0cecba7a2d371014db9d6b1f002
Gabriel Campanário <gabriel.camp96@hotmail.com>
2019-09-19 20:52:42 -0300

    Listando todos os casos com DTO

--- src/main/java/com/gabrielcamp/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/gabrielcamp/cursomc/resources/CategoriaResource.java
@@ -2,6 +2,8 @@ package com.gabrielcamp.cursomc.resources;
 
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gabrielcamp.cursomc.domain.Categoria;
+import com.gabrielcamp.cursomc.dto.CategoriaDTO;
 import com.gabrielcamp.cursomc.services.CategoriaService;
 
 // Responsável por instanciar e popular os objetos (Resource - Controlador REST)
@@ -49,5 +52,12 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();		
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);		
 	}
 }
