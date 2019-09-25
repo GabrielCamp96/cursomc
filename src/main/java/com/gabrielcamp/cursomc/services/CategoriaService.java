2cf1e2b75a453297319e3da48a5af7e0279e4310
Gabriel Campanário <gabriel.camp96@hotmail.com>
2019-09-25 08:42:05 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/gabrielcamp/cursomc/services/CategoriaService.java
+++ src/main/java/com/gabrielcamp/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.gabrielcamp.cursomc.domain.Categoria;
+import com.gabrielcamp.cursomc.dto.CategoriaDTO;
 import com.gabrielcamp.cursomc.repositories.CategoriaRepository;
 import com.gabrielcamp.cursomc.services.exceptions.DataIntegrityException;
 import com.gabrielcamp.cursomc.services.exceptions.ObjectNotFoundException;
@@ -57,6 +58,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
