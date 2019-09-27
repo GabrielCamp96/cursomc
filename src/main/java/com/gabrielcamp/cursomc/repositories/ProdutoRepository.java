e83d7cb7e110dec8e9f0160134ad532038c573b5
Gabriel Campanário <gabriel.camp96@hotmail.com>
2019-09-27 07:44:51 -0300

    Busca de pedidos por nome e categorias

--- src/main/java/com/gabrielcamp/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/gabrielcamp/cursomc/repositories/ProdutoRepository.java
@@ -1,8 +1,13 @@
 package com.gabrielcamp.cursomc.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.gabrielcamp.cursomc.domain.Categoria;
 import com.gabrielcamp.cursomc.domain.Produto;
 
 // Responsável por trabalhar e consultar o BD (Repository)
@@ -11,4 +16,8 @@ import com.gabrielcamp.cursomc.domain.Produto;
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 	
+//	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+//	Page<Produto> search(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
+
 }
